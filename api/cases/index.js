40c54fa923da7fb4ff779ad01fc22ad8731aca88
tobymoreton <toby@tmclegal.co.uk>
2026-06-22 06:49:56 +0100

    Update index.js

--- api/cases/index.js
+++ api/cases/index.js
@@ -1,16 +1,7 @@
-{
-  "bindings": [
-    {
-      "authLevel": "anonymous",
-      "type": "httpTrigger",
-      "direction": "in",
-      "name": "req",
-      "methods": ["get"]
-    },
-    {
-      "type": "http",
-      "direction": "out",
-      "name": "res"
-    }
-  ]
-}
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ value: [], test: true })
+  };
+};
